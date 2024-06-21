c03e2c070ad406c4290f1e2941a0e5bd28952c20
Phi Nguyen <phiwe3296@gmail.com>
2024-06-20 23:08:12 -0400

    cleaned up default files

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// src/index.js
+import React from "react";
+import { createRoot } from "react-dom";
+import App from "./App";
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
