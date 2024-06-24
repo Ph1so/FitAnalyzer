14ebe2b96d5546b17ccefd8899d19687e015450e
Phi Nguyen <phiwe3296@gmail.com>
2024-06-24 16:09:53 -0700

    added connection to api

--- src/exercises.py
+++ src/exercises.py
@@ -1,1 +1,20 @@
 import requests
+from dotenv import load_dotenv
+import os
+
+def configure():
+    load_dotenv()
+
+def get_exercises(muscle):
+    api_url = 'https://api.api-ninjas.com/v1/exercises?muscle={}'.format(muscle)
+    response = requests.get(api_url, headers={'X-Api-Key': os.getenv('api_key')})
+    if response.status_code == requests.codes.ok:
+        print(response.text)
+    else:
+        print("Error:", response.status_code, response.text)
+
+def main():
+    configure()
+    get_exercises('biceps')
+
+main()
