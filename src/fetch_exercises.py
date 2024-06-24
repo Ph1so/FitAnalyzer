eabcd8ae21e2086df030df896fb0f39f3f30ab81
yashjagtap23 <yash05jagtap@gmail.com>
2024-06-24 18:55:29 -0500

    added a fetch file
    fetches the data, can change based on what is in the muscle group variable. Need to hard code key tho.

--- src/fetch_exercises.py
+++ src/fetch_exercises.py
@@ -7,15 +7,17 @@ def configure():
 
 def get_exercises(muscle):
     api_url = 'https://api.api-ninjas.com/v1/exercises?muscle={}'.format(muscle)
-    response = requests.get(api_url, headers={'X-Api-Key': os.getenv('VLZrd4JluETX99Ojr4h06w==AmFEACNadX8JYxQK')})
+    response = requests.get(api_url, headers={'X-Api-Key': 'VLZrd4JluETX99Ojr4h06w==AmFEACNadX8JYxQK'})
     if response.status_code == requests.codes.ok:
         print(response.text)
     else:
         print("Error:", response.status_code, response.text)
 
+muscleGroupVar = "chest"
+
 def main():
     configure()
-    get_exercises('biceps')
+    get_exercises(muscleGroupVar)
 
-
-main()
+if __name__ == '__main__':
+    main()
