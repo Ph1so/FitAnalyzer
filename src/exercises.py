97c7a380b65d22f6d33150b14f99528d7adfa8d0
Phi Nguyen <phiwe3296@gmail.com>
2024-06-24 18:00:49 -0700

    cleaned exercises.py + commit random venv files

--- src/exercises.py
+++ src/exercises.py
@@ -7,19 +7,15 @@ def configure():
 
 def get_exercises(muscle):
     api_url = 'https://api.api-ninjas.com/v1/exercises?muscle={}'.format(muscle)
-    response = requests.get(api_url, headers={'X-Api-Key': os.getenv('VLZrd4JluETX99Ojr4h06w==AmFEACNadX8JYxQK')})
+    response = requests.get(api_url, headers={'X-Api-Key': os.getenv('api_key')})
     if response.status_code == requests.codes.ok:
         print(response.text)
     else:
         print(f"Error: {response.status_code} - {response.text}")
-    
-    return exercise_names
-
-muscleGroupVar = "chest"
 
 def main():
     configure()
-    get_exercises('incline hammer curls')
+    get_exercises('biceps')
 
 
 main()
