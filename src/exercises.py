494d08a04ab84d4c38a758917a45fcd1139f3d72
Phi Nguyen <phiwe3296@gmail.com>
2024-06-24 17:15:51 -0700

    colect all exercise names

--- src/exercises.py
+++ src/exercises.py
@@ -7,17 +7,19 @@ def configure():
 
 def get_exercises(muscle):
     api_url = 'https://api.api-ninjas.com/v1/exercises?muscle={}'.format(muscle)
-    response = requests.get(api_url, headers={'X-Api-Key': 'VLZrd4JluETX99Ojr4h06w==AmFEACNadX8JYxQK'})
+    response = requests.get(api_url, headers={'X-Api-Key': os.getenv('VLZrd4JluETX99Ojr4h06w==AmFEACNadX8JYxQK')})
     if response.status_code == requests.codes.ok:
         print(response.text)
     else:
-        print("Error:", response.status_code, response.text)
+        print(f"Error: {response.status_code} - {response.text}")
+    
+    return exercise_names
 
 muscleGroupVar = "chest"
 
 def main():
     configure()
-    get_exercises(muscleGroupVar)
+    get_exercises('incline hammer curls')
 
 
 main()
