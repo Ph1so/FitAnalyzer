bc93b73f262a9557ef923fe1974bd1804ca037f4
Phi Nguyen <phiwe3296@gmail.com>
2024-07-08 23:46:56 +0700

    fixed mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,17 +3,23 @@ import pluginJs from "@eslint/js";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 import { fixupConfigRules } from "@eslint/compat";
 
-export default [
-  { files: ["**/*.{js,mjs,cjs,jsx}"] },
-  { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  ...fixupConfigRules(pluginReactConfig),
-  {
-    settings: {
-      react: {
-        version: "detect",
+export default {
+  files: ["**/*.{js,mjs,cjs,jsx}"],
+  languageOptions: {
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
       },
     },
+    globals: globals.browser,
   },
-];
+  configs: [pluginJs.configs.recommended],
+  rules: {
+    ...fixupConfigRules(pluginReactConfig),
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
