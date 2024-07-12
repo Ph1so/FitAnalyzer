5419d1277ef748a749d8470f031182cc920e8ebd
Jokimessi21 <kimjosh2020@gmail.com>
2024-07-12 18:49:14 -0500

    rerouting so the button works correctly

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import "./index.css";
 import Title from "./components/Title";
 import WorkoutName from "./components/WorkoutName";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import LandingPage from './components/LandingPage';
 
 function App() {
   return (
-    <div className="App">
-      <div className="version">v.1.0</div>
-      <div className="Components-Container">
-        <Title />
-        <WorkoutName />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/workout" element={<WorkoutPage />} />
+        </Routes>
       </div>
-    </div>
+    </Router>
   );
 }
+
+const WorkoutPage = () => (
+  <div className="Components-Container">
+    <div className="version">v.1.0</div>
+    <Title />
+    <WorkoutName />
+  </div>
+);
 
 export default App;
