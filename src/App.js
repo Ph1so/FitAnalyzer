74fafc7b3f5b28918fdc4f0b42ddfd9b3db64e0e
yashjagtap23 <108370176+yashjagtap23@users.noreply.github.com>
2024-07-15 01:37:03 -0500

    Added Bar Chart
    For now jus added it to main page since nav bar wasn't ready. Also hard coded user values which appear in white.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from "react";
 import "./index.css";
 import Title from "./components/Title";
 import WorkoutName from "./components/WorkoutName";
-
+import {Chart as ChartJs } from 'chart.js/auto';
+import { Bar } from 'react-chartjs-2';
+import BarChart from "./components/BarChart";
 
 // TODO: consider putting the array of day names + workouts here instead of in WorkoutName.js and then passing it down as props
 function App() {
@@ -12,6 +14,9 @@ function App() {
       <div className="Components-Container">
         <Title />
         <WorkoutName />
+      </div>
+      <div>
+        <BarChart/>
       </div>
     </div>
   );
---
+++ src/components/BarChart.js
@@ -0,0 +1,71 @@
+import React from "react";
+import {Chart as ChartJs } from 'chart.js/auto';
+import { Bar } from 'react-chartjs-2';
+
+
+const BarChart = () => {
+  return (
+    <div>
+        <Bar 
+          data={{
+            labels: ["Back", "Bicep", "Tricep", "Chest", "Legs"],
+            datasets: [
+              {
+                label: "User",
+                data: [5, 5, 7, 10, 6],
+                backgroundColor: [
+                    '#FFFFFF'
+                ],
+                borderColor: "#ffffff",
+                tension: 0.4,
+                type:'scatter'
+              },
+              {
+                // later add variable so that user data is the sets they input 
+                label: "Sub-Optimal",
+                data: [12, 10, 8, 10, 10],
+                backgroundColor: [
+                  'rgba(231,188,64)'
+                ]
+              },
+              {
+                label: "Optimal",
+                data: [4, 5, 4, 6, 6],
+                backgroundColor: [
+                  '#00ab41'
+                ]
+              },
+              {
+                label: "Junk",
+                data: [4, 5, 8, 4, 4],
+                backgroundColor: [
+                  "#D7504D"
+                ]
+              },
+              
+            ],
+          }}
+          options={{
+            scales: {
+              x: {
+                stacked: true,
+                
+              },
+              y: {
+                beginAtZero: true,
+                grid: {
+                    color: '#808080',
+                },
+                stacked: true,
+              },
+              
+            }
+          }
+          }
+        />
+        </div>
+   
+  );
+};
+
+export default BarChart;
