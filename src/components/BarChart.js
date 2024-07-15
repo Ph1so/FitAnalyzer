60ab9910c0dbb6ff1176d174e8f089a38a4a3215
Phi Nguyen <phiwe3296@gmail.com>
2024-07-16 01:31:47 +0900

    style chart + repsonsive

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -1,82 +1,69 @@
 import React from "react";
-import { Bar } from 'react-chartjs-2';
-
+import { Bar } from "react-chartjs-2";
+import "./BarChart.css";
 
 const BarChart = () => {
   return (
-    <div>
-        <Bar 
-          data={{
-            labels: ["Back", "Bicep", "Tricep", "Chest", "Legs"],
-            datasets: [
-              {
-                label: "User",
-                data: [7, 18, 7, 12, 16],
-                backgroundColor: [
-                    '#FFFFFF'
-                ],
-                borderColor: "#ffffff",
-                tension: 0.4,
-                type:'scatter'
+    <div className="bar-chart-container">
+      <Bar
+        data={{
+          labels: ["Back", "Bicep", "Tricep", "Chest", "Legs"],
+          datasets: [
+            {
+              label: "User",
+              data: [7, 18, 7, 12, 16],
+              backgroundColor: ["#FFFFFF"],
+              borderColor: "#ffffff",
+              tension: 0.4,
+              type: "scatter",
+            },
+            {
+              label: "Sub-Optimal",
+              data: [12, 10, 8, 10, 10],
+              backgroundColor: ["rgba(231,188,64)"],
+              borderWidth: 1,
+              barPercentage: 0.7,
+              categoryPercentage: 0.5,
+            },
+            {
+              label: "Optimal",
+              data: [6, 5, 5, 6, 8],
+              backgroundColor: ["#00ab41"],
+              borderWidth: 1,
+              barPercentage: 0.7,
+              categoryPercentage: 0.5,
+            },
+            {
+              label: "Junk",
+              data: [2, 5, 7, 4, 2],
+              backgroundColor: ["#D7504D"],
+              borderWidth: 1,
+              barPercentage: 0.7,
+              categoryPercentage: 0.5,
+            },
+          ],
+        }}
+        options={{
+          indexAxis: "y",
+          scales: {
+            x: {
+              beginAtZero: true,
+              stacked: true,
+              grid: {
+                color: "#808080",
               },
-              {
-                // later add variable so that user data is the sets they input 
-                label: "Sub-Optimal",
-                data: [12, 10, 8, 10, 10],
-                backgroundColor: [
-                  'rgba(231,188,64)'
-                ],
-                borderWidth: 1,
-                barPercentage: 0.7,
-                categoryPercentage: 0.5
+            },
+            y: {
+              beginAtZero: true,
+              grid: {
+                color: "#808080",
               },
-              {
-                label: "Optimal",
-                data: [6, 5, 5, 6, 8],
-                backgroundColor: [
-                  '#00ab41'
-                ],
-                borderWidth: 1,
-                barPercentage: 0.7,
-                categoryPercentage: 0.5
-              },
-              {
-                label: "Junk",
-                data: [2, 5, 7, 4, 2],
-                backgroundColor: [
-                  "#D7504D"
-                ],
-                borderWidth: 1,
-                barPercentage: 0.7,
-                categoryPercentage: 0.5
-              },
-              
-            ],
-          }}
-          options={{
-            indexAxis: 'y',
-            scales: {
-              x: {
-                beginAtZero: true,
-                stacked: true,
-                grid: {
-                  color: '#808080',
-              },
-              },
-              y: {
-                beginAtZero: true,
-                grid: {
-                    color: '#808080',
-                },
-                stacked: true,
-              },
-              
-            }
-          }
-          }
-        />
-        </div>
-   
+              stacked: true,
+            },
+          },
+        }}
+      />
+    </div>
   );
 };
 
