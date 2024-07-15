38ca85a473c4e78fb8b2414c77923167ac6a8412
Phi Nguyen <phiwe3296@gmail.com>
2024-07-15 18:56:47 +0900

    fixed errors from merging

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from "react";
 import "./index.css";
 import Title from "./components/Title";
 import WorkoutName from "./components/WorkoutName";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import LandingPage from './components/LandingPage';
-import {Chart as ChartJs } from 'chart.js/auto'; //dont delete this otherwise it breaks 
-import { Bar } from 'react-chartjs-2';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import LandingPage from "./components/LandingPage";
+import { Chart as ChartJs } from "chart.js/auto"; //dont delete this otherwise it breaks
+import { Bar } from "react-chartjs-2";
 import BarChart from "./components/BarChart";
 
 // TODO: consider putting the array of day names + workouts here instead of in WorkoutName.js and then passing it down as props
@@ -21,7 +21,7 @@ function App() {
         </Routes>
       </div>
       <div>
-        <BarChart/>
+        <BarChart />
       </div>
     </Router>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,25 @@
-import React from 'react';
-import './Navbar.css';
-import logo from './logo.webp';
-import { FaHome } from "react-icons/fa";
-import { GiMuscleUp } from "react-icons/gi";
-import { SiSimpleanalytics } from "react-icons/si";
+import React from "react";
+import "./Navbar.css";
+import logo from "../images/logo.webp";
+// import { FaHome } from "react-icons/fa";
+// import { GiMuscleUp } from "react-icons/gi";
+// import { SiSimpleanalytics } from "react-icons/si";
 
 const Navbar = () => {
-    return (
-        <nav className="navbar">
-            <img src={logo} alt="FitAnalyzer Logo" className="navbar-logo" />
-            <h1 className="navbar-title">FitAnalyzer</h1>
-            <div className="icon-container">
-                <FaHome className="icon" />
-                <GiMuscleUp className="icon" />
-                <SiSimpleanalytics className="icon" />
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="navbar">
+      <img src={logo} alt="FitAnalyzer Logo" className="navbar-logo" />
+      <h1 className="navbar-title">FitAnalyzer</h1>
+      <div className="icon-container">
+        {/* <FaHome className="icon" />
+        <GiMuscleUp className="icon" />
+        <SiSimpleanalytics className="icon" /> */}
+        <a href="/" className="navbar-links">
+          Home
+        </a>
+      </div>
+    </nav>
+  );
 };
 
-export default Navbar
+export default Navbar;
