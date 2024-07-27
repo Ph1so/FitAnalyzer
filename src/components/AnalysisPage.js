db32abd7803805e084b1e6e4652ad8cfcbe29bf5
yashjagtap23 <108370176+yashjagtap23@users.noreply.github.com>
2024-07-27 14:02:45 -0500

    Added source Footer

--- src/components/AnalysisPage.js
+++ src/components/AnalysisPage.js
@@ -4,15 +4,15 @@ import { Chart as ChartJs } from "chart.js/auto";
 import { Bar } from "react-chartjs-2";
 import BarChart from "./BarChart";
 
-
 const AnalysisPage = () => {
-  
-  
   return (
     <div className="analysis-page">
-      <div>
+      <div className="content">
         <BarChart />
       </div>
+      <footer className="source">
+        <p>Source: <a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5684266/" target="_blank" rel="noopener noreferrer">Research on Exercise and Muscle Groups</a></p>
+      </footer>
     </div>
   );
 };
