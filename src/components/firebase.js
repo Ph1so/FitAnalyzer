cd0c3f265f8a0d71f97bf8e91dac6581b3cb9781
yashjagtap23 <108370176+yashjagtap23@users.noreply.github.com>
2024-08-08 18:31:49 -0500

    secured firebase.js changed projects

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,26 +1,21 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Access environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyAmsV0DeviP4AuLH10NoK6XHw-BG9oPNqg",
-  authDomain: "fitnessapp-e8b53.firebaseapp.com",
-  projectId: "fitnessapp-e8b53",
-  storageBucket: "fitnessapp-e8b53.appspot.com",
-  messagingSenderId: "217165628371",
-  appId: "1:217165628371:web:f7b5f33b07ec9b009fe526",
-  measurementId: "G-9RB7Y94KBL"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-
 const analytics = getAnalytics(app);
 
 export { db };
