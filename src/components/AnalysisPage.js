d12d22f69f596f581bdd1aee494d6187e8e77fda
yashjagtap23 <108370176+yashjagtap23@users.noreply.github.com>
2024-08-13 12:50:38 -0500

    Added return to planner button

--- src/components/AnalysisPage.js
+++ src/components/AnalysisPage.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import './AnalysisPage.css';
-import { Chart as ChartJs } from "chart.js/auto";
-import { Bar } from "react-chartjs-2";
 import BarChart from "./BarChart";
+import { useNavigate } from "react-router-dom";
 
 const AnalysisPage = () => {
+
+  const navigate = useNavigate();
+
+  const handlePlannerClick = () => {
+    navigate("/workout"); // Navigate to planner page
+  };
+
   return (
     <div className="analysis-page">
       <div className="content">
         <BarChart />
+      </div>
+      <div className="Planner-Button-Container">
+        <button 
+          className="Planner-Button" 
+          onClick={handlePlannerClick}
+        >
+          Return to Planner
+        </button>
       </div>
       <footer className="source">
         <p>Source: <a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5684266/" target="_blank" rel="noopener noreferrer">Research on Exercise and Muscle Groups</a></p>
